76d022c5b67a00b8a89a9ce4c405f7e023c2d3ee
Isaac To <candleindark@gmail.com>
2018-08-26 19:40:59 +0000

    Complete part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -39,15 +39,15 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[[:ascii:]&&[:alpha:]&&[^AEIOUaeiou]]/ ? true : false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^(?:0+|(?:[01]+00))$/ ? true : false
 end
 
 # Part 3
